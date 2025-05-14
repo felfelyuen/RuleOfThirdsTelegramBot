f6424f26d2fe7167c9aa02072eb81d1e6eb2b04c
FelixYuen <e1121462@u.nus.edu>
2025-05-14 12:40:46 +0800

    Added some basic code Commiting now in case IDE screws up somehow

---
+++ .idea/camera.py
@@ -0,0 +1,13 @@
+class Camera:
+    brand
+    model
+    japanese
+    battery
+    megapixel
+
+    def __init__(self, brand, model, japanese, battery, megapixel):
+        self.brand = brand
+        self.model = model
+        self.japanese = japanese
+        self.battery = battery
+        self.megapixel = megapixel
